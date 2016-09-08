141b7e98e58ae732d975a918ff9759b9cec93ed6
runner <runner@dinky.monkey.island>
2016-09-08 04:18:43 +0200

    save current state

--- module/PlanetSudoStrategyPack/src/main/java/org/openbase/planetsudo/game/strategy/StrategieNoxus.java
+++ module/PlanetSudoStrategyPack/src/main/java/org/openbase/planetsudo/game/strategy/StrategieNoxus.java
@@ -5,6 +5,28 @@
 
 package org.openbase.planetsudo.game.strategy;
 
+/*-
+ * #%L
+ * PlanetSudo GameEngine
+ * %%
+ * Copyright (C) 2009 - 2016 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import org.openbase.planetsudo.level.levelobjects.AgentInterface;
 import org.openbase.planetsudo.level.levelobjects.Resource;
 
@@ -21,7 +43,7 @@ public class StrategieNoxus extends AbstractStrategy {
 	}
 
 	/**
-	 * Wie viele Agenten sollen erstellt werde wird hier angegeben.
+	 * Hier wird angegeben wie viele Agenten dem Team zur Verfügung stehen sollen.
 	 * @return Anzahl der Agenten
 	 */
 	@Override
