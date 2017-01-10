8ea6b2ea7c125a1436ae793e7db4f5d85ab2b267
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-01-11 00:34:41 +0100

    License header 2017 upgrade.

--- src/main/java/org/openbase/planetsudo/jp/JPStrategyModuleDirectory.java
+++ src/main/java/org/openbase/planetsudo/jp/JPStrategyModuleDirectory.java
@@ -9,7 +9,7 @@ package org.openbase.planetsudo.jp;
  * #%L
  * PlanetSudo Server
  * %%
- * Copyright (C) 2009 - 2016 openbase.org
+ * Copyright (C) 2009 - 2017 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
